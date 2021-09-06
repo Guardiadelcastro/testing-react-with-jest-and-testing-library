411b282189dc051569c1b738f7708b82f1a633b6
guardiadelcastro <guardiadelcastro@gmail.com>
2021-09-06 10:33:50 +0100

    test OrderEntry

---
+++ sundaes-app/src/pages/common/AlertBanner.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { Alert } from 'react-bootstrap'
+
+export default function AlertBanner({
+  message = 'An unexpected error ocurred. Please try again later',
+  variant = 'danger',
+}) {
+  return (
+    <Alert variant={variant} style={{ backgroundColor: 'red' }}>
+      {message}
+    </Alert>
+  )
+}
--- sundaes-app/src/pages/entry/Options.jsx
+++ sundaes-app/src/pages/entry/Options.jsx
@@ -3,9 +3,11 @@ import axios from 'axios'
 import ScoopOption from './ScoopOption'
 import ToppingOption from './ToppingOption'
 import { Row } from 'react-bootstrap'
+import AlertBanner from '../common/AlertBanner'
 
 export default function Options({ optionType }) {
   const [items, setItems] = React.useState([])
+  const [error, setError] = React.useState(false)
   React.useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
@@ -13,9 +15,13 @@ export default function Options({ optionType }) {
         setItems(data)
       })
       .catch((error) => {
-        //TODO: handle error
+        setError(true)
       })
   }, [optionType])
+
+  if (error) {
+    return <AlertBanner />
+  }
 
   const ItemComponent = {
     scoops: ScoopOption,
